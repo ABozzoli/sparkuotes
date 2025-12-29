d176613890e35111bf488465063f23cf5e2eb716
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-29 23:53:40 +0100

    feat(button-component): make it default to type "button", make onClick prop optional

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -6,15 +6,24 @@ import { ComponentProps, ReactNode } from "react";
 
 interface Props extends ComponentProps<"button"> {
   children: ReactNode;
-  onClick: () => void;
+  type?: "submit" | "reset" | "button";
+  onClick?: () => void;
   iconBefore?: string;
   iconAfter?: string;
   iconSize?: string | number;
 }
 
-export default function Button({ children, onClick, iconBefore, iconAfter, iconSize = "1em", ...props }: Props) {
+export default function Button({
+  children,
+  type = "button",
+  onClick,
+  iconBefore,
+  iconAfter,
+  iconSize = "1em",
+  ...props
+}: Props) {
   return (
-    <button className={styles.button} onClick={onClick} {...props}>
+    <button className={styles.button} type={type} onClick={onClick} {...props}>
       {iconBefore && <Icon name={iconBefore} size={iconSize} />}
       {children}
       {iconAfter && <Icon name={iconAfter} size={iconSize} />}
