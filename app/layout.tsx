b2a9dc07aa4e158dab9ff1d66c24a2f7c654c863
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-29 22:15:06 +0100

    feat(styles): add normalize.css and a font variables sheet, add some basic styles/resets and utilities to globals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../css/fonts.css";
+import "../css/globals.css";
+import "../css/normalize.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
