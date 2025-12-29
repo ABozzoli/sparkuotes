eaa7f3962d94cdef6600037b251d0da7db974592
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-29 22:21:22 +0100

    feat(icon-component): add Icon component with svg sprite integration

---
+++ components/icon/icon.tsx
@@ -0,0 +1,15 @@
+import styles from "./icon.module.css";
+import { ComponentProps } from "react";
+
+interface Props extends ComponentProps<"svg"> {
+  name: string;
+  size?: string | number;
+}
+
+export default function Icon({ name, size = "1em", ...props }: Props) {
+  return (
+    <svg className={styles.icon} {...props} width={size} height={size} aria-hidden={true} focusable={false}>
+      <use href={`/spritesheet.svg#icon-${name}`} />
+    </svg>
+  );
+}
