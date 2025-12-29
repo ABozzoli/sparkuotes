be0b7d3f249707e96f3e985a43571934d4189ae4
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-29 22:49:59 +0100

    feat(styles): add colors.css for color variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "../css/colors.css";
 import "../css/fonts.css";
 import "../css/globals.css";
 import "../css/normalize.css";
