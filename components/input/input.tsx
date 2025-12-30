af4469629101599757cf70040ad8647a9496b858
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-30 19:11:18 +0100

    feat(input-component): add prop "hiddenLabel", extend visually-hidden css utility class to be able to use data-attribute directly on component tags

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -4,27 +4,29 @@ import Label from "../label/label";
 
 type InputProps = ComponentProps<"input"> & {
   label: string;
+  hiddenLabel?: boolean;
   multiline?: false;
 };
 
 type TextareaProps = ComponentProps<"textarea"> & {
   label: string;
+  hiddenLabel?: boolean;
   multiline: true;
 };
 
 type Props = InputProps | TextareaProps;
 
 /* Note: 'as any' is needed for dynamic element props. Type safety is still maintained at the component's public API level through the Props type union */
-export default function Input({ children, required, label, multiline, ...props }: Props) {
+export default function Input({ children, required, label, hiddenLabel, multiline, ...props }: Props) {
   const id = useId();
   const Element = multiline ? "textarea" : "input";
 
   return (
     <div className={styles["input-wrapper"]}>
-      <Label required={required} htmlFor={id}>
+      <Label required={required} htmlFor={id} data-visually-hidden={hiddenLabel ? "true" : undefined}>
         {label}
       </Label>
-      <Element id={id} className={styles.input} required={required} {...props as any} />
+      <Element id={id} className={styles.input} required={required} {...(props as any)} />
     </div>
   );
 }
