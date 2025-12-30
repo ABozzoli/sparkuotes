87deb07ef6882a552ab746f7c84b17dab9502063
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-30 10:25:42 +0100

    feat(home-page): add sparkuotes logo, make h1 visually-hidden

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import styles from "./page.module.css";
+import Image from "next/image";
 import AddQuote from "@/components/add-quotes/add-quote.tsx";
 
 export default function Home() {
   return (
     <div className="content-wrapper">
-      <h1>Sparkuotes</h1>
+      <h1 className="visually-hidden">Sparkuotes</h1>
+
+      <div className={styles["logo-wrapper"]}>
+        <Image src="/sparkuotes-logo.svg" width={546.05} height={80} alt="Sparkuotes logo" />
+      </div>
 
       <AddQuote />
     </div>
