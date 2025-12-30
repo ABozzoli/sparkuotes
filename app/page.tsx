87037cc972f205b04abfba6808c916a168202031
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-30 15:43:58 +0100

    chore(add-quote-component/home-page): delete add-quote component and move its code in the home page directly for easier handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import AddQuote from "@/components/add-quotes/add-quote.tsx";
+import Input from "@/components/input/input";
+import Button from "@/components/button/button";
 
 export default function Home() {
   return (
@@ -11,7 +12,13 @@ export default function Home() {
         <Image src="/sparkuotes-logo.svg" width={546.05} height={80} alt="Sparkuotes logo" />
       </div>
 
-      <AddQuote />
+      <form className={styles["add-quote"]} aria-label="Add a new quote">
+        <Input label="Author" />
+        <Input label="Quote" multiline required />
+        <Button type="submit" iconBefore="plus" iconSize="1.5rem">
+          Add quote
+        </Button>
+      </form>
     </div>
   );
 }
