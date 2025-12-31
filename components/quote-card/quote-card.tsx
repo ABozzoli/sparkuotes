04033f6bfea929d41dda1308d437ba55caa686f3
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-31 21:03:53 +0100

    feat(quote-card-component): introduce "saved" and "suggested" variants with dedicated action buttons

--- components/quote-card/quote-card.tsx
+++ components/quote-card/quote-card.tsx
@@ -1,15 +1,55 @@
 import Quote from "@/components/quote/quote";
 import { QuoteI } from "@/components/quote/quote.types";
 import CopyButton from "@/components/copy-button/copy-button";
+import Button from "@/components/button/button";
 import styles from "./quote-card.module.css";
 
-interface Props extends QuoteI {}
+type SavedVariantProps = {
+  variant: "saved";
+  onDelete?: () => void;
+  onAdd?: never;
+  onRefresh?: never;
+};
 
-export default function QuoteCard({ text, author }: Props) {
+type SuggestedVariantProps = {
+  variant: "suggested";
+  onAdd: () => void;
+  onRefresh: () => void;
+  onDelete?: never;
+};
+
+type Props = QuoteI & (SavedVariantProps | SuggestedVariantProps);
+
+export default function QuoteCard({ text, author, variant, onDelete, onAdd, onRefresh }: Props) {
   return (
     <div className={styles["quote-card"]}>
       <Quote text={text} author={author} />
-      <CopyButton text={text} author={author} />
+      <div className={styles["actions"]}>
+        {variant === "saved" && (
+          <>
+            <CopyButton text={text} author={author} />
+            {onDelete && (
+              <Button iconBefore="trash-03" variant="secondary" onClick={onDelete} hiddenLabel>
+                Delete this quote
+              </Button>
+            )}
+          </>
+        )}
+        {variant === "suggested" && (
+          <>
+            {onAdd && (
+              <Button iconBefore="plus" onClick={onAdd} hiddenLabel>
+                Add this quote
+              </Button>
+            )}
+            {onRefresh && (
+              <Button iconBefore="refresh-cw-02" variant="secondary" onClick={onRefresh} hiddenLabel>
+                New suggestion
+              </Button>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
