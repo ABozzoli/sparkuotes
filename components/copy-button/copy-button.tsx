4373dd267e8d1dd2304a0e60adc951268cf534d6
ABozzoli <alessandrobozzoli92@gmail.com>
2025-12-31 01:23:34 +0100

    fix(copy-button-component): provide fallback for copy-to-clipboard functionality if "writeText" function isn't available

--- components/copy-button/copy-button.tsx
+++ components/copy-button/copy-button.tsx
@@ -11,11 +11,26 @@ interface CopyButtonProps {
 export default function CopyButton({ quote, author }: CopyButtonProps) {
   const [copied, setCopied] = useState(false);
 
+  const fallbackCopy = (text: string) => {
+    const textarea = Object.assign(document.createElement("textarea"), {
+      value: text,
+      style: "position:fixed;left:-999999px;top:-999999px",
+    });
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  };
+
   const copyToClipboard = async () => {
     const formattedQuote = author ? `${quote}\n(${author})` : quote;
 
     try {
-      await navigator.clipboard.writeText(formattedQuote);
+      if (navigator.clipboard?.writeText) {
+        await navigator.clipboard.writeText(formattedQuote);
+      } else {
+        fallbackCopy(formattedQuote);
+      }
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     } catch (err) {
